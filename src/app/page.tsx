3ad7980553633d1dba5b87064c08703573fd3571
kakizawaayato <chibairodori25@gmail.com>
2023-05-21 11:38:49 +0900

    ✨TOPページにURL一覧記載

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import { HomeTemplate } from '@/components/home/homeTemplate';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const HomePage: NextPage = (): JSX.Element => {
   return (
     <>
-      {/* TODO タイトルやメタディスクリプション(13.4の新機能がある) */}
-      <HomeTemplate />
+      <h2>AppRouter</h2>
+      <ul className="pl-4">
+        <li>
+          <Link href="/app-trial-a">/app-trial-a</Link>
+        </li>
+        <li>
+          <Link href="/app-trial-b">/app-trial-b</Link>
+        </li>
+      </ul>
+      <h2>PagesRouter</h2>
+      <ul className="pl-4">
+        <li>
+          <Link href="/page-trial-a">/page-trial-a</Link>
+        </li>
+        <li>
+          <Link href="/page-trial-b">/page-trial-b</Link>
+        </li>
+      </ul>
     </>
   );
 };
